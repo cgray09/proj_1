c51ba0978056ddd440313b18ea7548235eb0d8ed
cgray009 <cgray009@student.ucr.deu>
2020-09-18 13:30:09 -0700

    back

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID: '395713993395-itr9t8360hvnbv5haki2o7j374er3qnm.apps.googleusercontent.com',
+  googleClientSecret: 'a12atFecq3BzQw8W6bU7R-h6',
+  mongoURI: 'mongodb+srv://cgray009:Iwillfollow7@demo-cluster-40t16.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'ajdljlsiepirwcxcmcxjflsisipropw',
+  stripePublishableKey: 'pk_test_51HEdWoCf4RBlO79Rg1QklXntzVcq1UeEH7VXBE1bUk3VWJb5uNzAPmlKaVSFf1jcXgvB2rSWihRGfogUQbH9lBNZ00lKRMyMtA',
+  stripeSecretKey: 'sk_test_51HEdWoCf4RBlO79RiMHRUwEJjTJRMYo9pIxm37V7lhh3mwJ57WVUvvkyFUh3gBBRtvbCW31LHBqsJY3WwSTVoPIk00BrPnfaZZ',
+  sendGridKey: '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',
+  redirectDomain: 'http://localhost:3000'
+};
+
